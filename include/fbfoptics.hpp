94c67fb6907616906d213b2659adb70d8016e66c
Jonathan Bramble <j.p.bramble@leeds.ac.uk>
2011-11-11 17:27:39 +0000

    comments

--- include/fbfoptics.hpp
+++ include/fbfoptics.hpp
@@ -1,12 +1,3 @@
-/*
- * @file fbfoptics.hpp
- * @brief All the code required to do 4x4 optics on stratified media. Following the methods of Berreman. Can be used for both isotropic and aniostropic layers, but entry and exit media must be isotropic. isotropic layers tested, but aniostropic not yet tested
- * 
- * 
- * @author Jonathan Bramble
- * @date 25/06/2009
- */
-
 #ifndef FBFOPTICS_H_
 #define FBFOPTICS_H_
 
@@ -24,20 +15,31 @@
 
 using namespace boost::numeric::ublas;
 using namespace std;
-
-
-
+/*
+ * @file fbfoptics.hpp
+ * @brief All the code required to do 4x4 optics on stratified media. Following the methods of Berreman. Can be used for both isotropic and aniostropic layers, but entry and exit media must be isotropic. isotropic layers tested, but aniostropic not yet tested
+ * 
+ * 
+ * @author Jonathan Bramble
+ * @date 25/06/2009
+ */
 class Fbfoptics {
 
 public:
 	/// The entrance matrix
 	static void incmat(const double, const double, matrix<complex<double> >&);
+	/// The exit matrix
 	static void extmat(const double, const complex<double>, matrix<complex<double> >&);
+	/// The dielectric tensor
 	static void dietens(const double,const double,const double,const double,const double,const double, const double, matrix<double>&);
+	/// The general transfer matrix for isotropic layers
 	static void gtmiso(const complex<double>,const double,const double,const double, matrix<complex<double> >&);
-	static void diffpropmat(const matrix<double> ep, const double eta, matrix<double>& Delta);
-	static double rpp(const matrix<complex<double> >& M);
+	/// The general transfer matrix for anisotropic layers
 	static void gtm(const matrix<double>& Delta, const double k0, const double h, matrix<complex<double> >& T);
+	/// The differential propagation matrix Delta
+	static void diffpropmat(const matrix<double> ep, const double eta, matrix<double>& Delta);
+	/// Calculates from the total transfer matrix the rpp reflectivity co-efficient
+	static double rpp(const matrix<complex<double> >& M);
 };
 
 #endif /* FBFOPTICS_H_ */
